6f3251bede8e6dc86f8450014a3c917345472052
dallingilbert <djgilb24@byui.edu>
2025-01-27 13:40:27 -0800

    Login body div to fix style

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -13,51 +13,53 @@ export default function Login(){
 
     return (
         <>
-            <div className="logo-header">
-                <OneHealthLogo />
-                <h1>ONEHEALTH</h1>
-            </div>
-            <div className="login-card">
-                <div className="login-form">
-                    <form action="">
-                        <input 
-                            type="email"
-                            placeholder='Email' 
-                            name="email" 
-                            value={email} 
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <input 
-                            type="password" 
-                            placeholder='Password'
-                            name="password" 
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <div className="user-help"> 
+            <div className="login-body">
+                <div className="logo-header">
+                    <OneHealthLogo />
+                    <h1>ONEHEALTH</h1>
+                </div>
+                <div className="login-card">
+                    <div className="login-form">
+                        <form action="">
                             <input 
-                                type="checkbox" 
-                                name="remember" 
-                                id="remember" 
+                                type="email"
+                                placeholder='Email' 
+                                name="email" 
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)}
                             />
-                            <label htmlFor="remember">
-                                Remember me
-                            </label>
-                            <a href="http://" target="_blank" rel="noopener noreferrer">
-                                Forgot Password?
-                            </a>
-                        </div>
-                        <button 
-                            className="login-button" 
-                            type="button" 
-                            onSubmit={handleLogin}>
-                        Login
-                        </button>
-                        <div className="account-creation">
-                            <p>Don't have an account?</p>
-                            <a href="#create-account">Create one</a>
-                        </div>
-                    </form>
+                            <input 
+                                type="password" 
+                                placeholder='Password'
+                                name="password" 
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <div className="user-help"> 
+                                <input 
+                                    type="checkbox" 
+                                    name="remember" 
+                                    id="remember" 
+                                />
+                                <label htmlFor="remember">
+                                    Remember me
+                                </label>
+                                <a href="http://" target="_blank" rel="noopener noreferrer">
+                                    Forgot Password?
+                                </a>
+                            </div>
+                            <button 
+                                className="login-button" 
+                                type="button" 
+                                onSubmit={handleLogin}>
+                            Login
+                            </button>
+                            <div className="account-creation">
+                                <p>Don't have an account?</p>
+                                <a href="#create-account">Create one</a>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
